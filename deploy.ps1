c7ada301b36b827f5d7ce9f7fe7d5a1949f7991d
Tkarp87 <Tkarp87@Live.com>
2017-11-04 15:00:14 -0700

    Add files via upload

--- deploy.ps1
+++ deploy.ps1
@@ -1,29 +1,3 @@
-<#
- .SYNOPSIS
-    Deploys a template to Azure
-
- .DESCRIPTION
-    Deploys an Azure Resource Manager template
-
- .PARAMETER subscriptionId
-    The subscription id where the template will be deployed.
-
- .PARAMETER resourceGroupName
-    The resource group where the template will be deployed. Can be the name of an existing or a new resource group.
-
- .PARAMETER resourceGroupLocation
-    Optional, a resource group location. If specified, will try to create a new resource group in this location. If not specified, assumes resource group is existing.
-
- .PARAMETER deploymentName
-    The deployment name.
-
- .PARAMETER templateFilePath
-    Optional, path to the template file. Defaults to template.json.
-
- .PARAMETER parametersFilePath
-    Optional, path to the parameters file. Defaults to parameters.json. If file is not found, will prompt for parameter values based on template.
-#>
-
 param(
  [Parameter(Mandatory=$True)]
  [string]
@@ -47,10 +21,7 @@ param(
  $parametersFilePath = "parameters.json"
 )
 
-<#
-.SYNOPSIS
-    Registers RPs
-#>
+
 Function RegisterRP {
     Param(
         [string]$ResourceProviderNamespace
